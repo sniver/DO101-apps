d665e82807e8d20ececb0adcaff575b0372878e8
mmahdy <mmahdy@kn-it.com>
2022-06-07 12:18:25 +0200

    change hello world version

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('this is version 3 of hello world!\n');
+  res.send('this is version 4 of hello world!\n');
 });
 
 app.listen(8080, function () {
